00e29da39553dd211d4457069547af3926a2536e
meissparker <meissparker@gmail.com>
2024-08-04 20:07:22 -0500

    first commit

---
+++ Arithmetic Operations in Daily Life.py
@@ -0,0 +1,9 @@
+# Task 1
+
+bread = 3.50
+
+peanut_butter = 2.50
+
+jelly = 4.25
+
+print(bread + peanut_butter + jelly)
---
+++ Showcase Your Dance Moves.py
@@ -0,0 +1,10 @@
+# Task 1 
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else: 
+    print("Take an umbrella!")
+
+    print(weather)
