118d03b55410b2bb254de866229c07215928d9b4
meissparker <meissparker@gmail.com>
2024-08-04 20:11:56 -0500

    first commit

--- Arithmetic Operations in Daily Life.py
+++ Arithmetic Operations in Daily Life.py
@@ -7,3 +7,11 @@ peanut_butter = 2.50
 jelly = 4.25
 
 print(bread + peanut_butter + jelly)
+
+# Task 2  
+
+initial_amount = 10000
+
+yearly_interest_rate = (.07 * initial_amount)
+
+print(initial_amount + yearly_interest_rate)
